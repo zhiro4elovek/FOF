4027f3c2398b9014561e7ed2568f59ce7078034f
Zhiro4elovek <serduksaha@yandex.ru>
2018-04-17 09:27:22 +0300

    All
    all

---
+++ Source/FOF/FOF.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FOF.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FOF, "FOF" );
---
+++ Source/FOF/none.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "none.h"
+
+none::none()
+{
+}
+
+none::~none()
+{
+}
